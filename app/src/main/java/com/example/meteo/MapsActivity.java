bf7b2d2b13be537fa81771165ab9cacca26ee3e4
Amina Maakoul <maakoulamina@gmail.com>
2022-05-20 11:55:29 +0100

    Météo App

---
+++ app/src/main/java/com/example/meteo/MapsActivity.java
@@ -0,0 +1,91 @@
+package com.example.meteo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+
+        Intent intent = getIntent();
+        Double lat = Double.parseDouble(intent.getStringExtra("lat"));
+        Double longi = Double.parseDouble(intent.getStringExtra("longi"));
+
+        LatLng city = new LatLng(lat, longi);
+        mMap.addMarker(new MarkerOptions().position(city).title("Marker in City"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(city));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(city,12));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            final GoogleMap gmap=mMap;
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                gmap.addMarker(new MarkerOptions().position(latLng));
+                Toast.makeText(MapsActivity.this, latLng.latitude+"-"
+                        +latLng.longitude, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Change the map type based on the user's selection.
+        switch (item.getItemId()) {
+            case R.id.normal_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.satellite_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
